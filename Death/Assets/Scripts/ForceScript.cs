6f6c82fa37ab5e340ff55222339f211e70221914
Avgustin4ik <32969119+Avgustin4ik@users.noreply.github.com>
2019-02-13 23:02:49 +0300

    Сделал нормально голову плохому пацану
    Нужно ещё разделить на голову и тело

--- Death/Assets/Scripts/ForceScript.cs
+++ Death/Assets/Scripts/ForceScript.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ForceScript : MonoBehaviour {
-
 	private bool useForce = true;
 	private float timeLife = 0.0f;
 	private const float maxTimeLife = 3.0f;
@@ -17,6 +16,8 @@ public class ForceScript : MonoBehaviour {
 	void Update () {
 		if(useForce)
 		{
+			GetComponent<Animator>().enabled = false;
+			GetComponent<SpriteRenderer>().sprite = GetComponent<HeadsMain>().rip;
 			transform.parent = null;
 			GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
 			GetComponent<Rigidbody2D>().AddForce(new Vector2(1.0f, Random.Range(0.0f, 1.0f)) * Random.Range(10.0f, 20.0f), ForceMode2D.Impulse);
