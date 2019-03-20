ddbef226ab996875edfa3e79602387ef4cab7cfb
Avgustin4ik <32969119+Avgustin4ik@users.noreply.github.com>
2019-03-20 23:19:21 +0300

    Добавил деревенские бэкграунды
    Опять же. Все в лоб и сам ничего не рисовал

--- Death/Assets/Scripts/BackgroundController.cs
+++ Death/Assets/Scripts/BackgroundController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BackgroundController : MonoBehaviour {
 	public float backgroundSpeed = 10f;
 	public GameObject[] screens = new GameObject[2];
-	public float whide = 60f;
+	public float whide;
 	// Use this for initialization
 	public float xLeft;
 	void Start () {
@@ -16,7 +16,7 @@ public class BackgroundController : MonoBehaviour {
 	void Update () {
 		transform.position = new Vector3(transform.position.x - Time.deltaTime * backgroundSpeed, transform.position.y, transform.position.z);
 		xLeft = screens[0].transform.position.x;
-		if(xLeft < -60f)
+		if(xLeft < -whide)
 		{
 			screens[0].transform.position = new Vector3(screens[1].transform.position.x + whide,screens[1].transform.position.y,screens[1].transform.position.z);
 			GameObject temp = screens[1];
